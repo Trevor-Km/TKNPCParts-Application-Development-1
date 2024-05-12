abd7954068a2f534217c42ff7e92d212a3f141eb
Trevor-Km <trevorkmcintyre@gmail.com>
2024-05-12 00:43:57 -0400

    Layout and main page

---
+++ TKN-PC-Layout/TKNLayout.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TKN_PC_Layout
+{
+    partial class TKNLayout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pCPartsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphicsCardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.motherBoardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.storageDevicesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.peripheralsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.termsAndConditionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userManualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.footerText = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pCPartsToolStripMenuItem,
+            this.infoToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pCPartsToolStripMenuItem
+            // 
+            this.pCPartsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem,
+            this.graphicsCardsToolStripMenuItem,
+            this.motherBoardsToolStripMenuItem,
+            this.storageDevicesToolStripMenuItem,
+            this.processorsToolStripMenuItem,
+            this.peripheralsToolStripMenuItem});
+            this.pCPartsToolStripMenuItem.Name = "pCPartsToolStripMenuItem";
+            this.pCPartsToolStripMenuItem.Size = new System.Drawing.Size(75, 26);
+            this.pCPartsToolStripMenuItem.Text = "PC Parts";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.mainToolStripMenuItem.Text = "&Main";
+            // 
+            // graphicsCardsToolStripMenuItem
+            // 
+            this.graphicsCardsToolStripMenuItem.Name = "graphicsCardsToolStripMenuItem";
+            this.graphicsCardsToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.graphicsCardsToolStripMenuItem.Text = "&Graphics Cards";
+            // 
+            // motherBoardsToolStripMenuItem
+            // 
+            this.motherBoardsToolStripMenuItem.Name = "motherBoardsToolStripMenuItem";
+            this.motherBoardsToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.motherBoardsToolStripMenuItem.Text = "M&otherBoards";
+            // 
+            // storageDevicesToolStripMenuItem
+            // 
+            this.storageDevicesToolStripMenuItem.Name = "storageDevicesToolStripMenuItem";
+            this.storageDevicesToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.storageDevicesToolStripMenuItem.Text = "&Storage Devices";
+            // 
+            // processorsToolStripMenuItem
+            // 
+            this.processorsToolStripMenuItem.Name = "processorsToolStripMenuItem";
+            this.processorsToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.processorsToolStripMenuItem.Text = "&Processors";
+            // 
+            // peripheralsToolStripMenuItem
+            // 
+            this.peripheralsToolStripMenuItem.Name = "peripheralsToolStripMenuItem";
+            this.peripheralsToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.peripheralsToolStripMenuItem.Text = "P&eripherals";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.termsAndConditionsToolStripMenuItem,
+            this.userManualToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(49, 26);
+            this.infoToolStripMenuItem.Text = "&Info";
+            // 
+            // termsAndConditionsToolStripMenuItem
+            // 
+            this.termsAndConditionsToolStripMenuItem.Name = "termsAndConditionsToolStripMenuItem";
+            this.termsAndConditionsToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
+            this.termsAndConditionsToolStripMenuItem.Text = "Terms and Conditions";
+            // 
+            // userManualToolStripMenuItem
+            // 
+            this.userManualToolStripMenuItem.Name = "userManualToolStripMenuItem";
+            this.userManualToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
+            this.userManualToolStripMenuItem.Text = "User Manual";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 26);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // logoBox
+            // 
+            this.logoBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.logoBox.Image = global::TKN_PC_Layout.Properties.Resources.TKNPCLOGO1;
+            this.logoBox.InitialImage = global::TKN_PC_Layout.Properties.Resources.TKNPCLOGO1;
+            this.logoBox.Location = new System.Drawing.Point(680, 0);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(120, 100);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoBox.TabIndex = 1;
+            this.logoBox.TabStop = false;
+            // 
+            // footerText
+            // 
+            this.footerText.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.footerText.AutoSize = true;
+            this.footerText.Location = new System.Drawing.Point(305, 420);
+            this.footerText.Name = "footerText";
+            this.footerText.Size = new System.Drawing.Size(191, 16);
+            this.footerText.TabIndex = 2;
+            this.footerText.Text = "Copyright TKN PC Parts 2024 ©";
+            // 
+            // TKNLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.footerText);
+            this.Controls.Add(this.logoBox);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TKNLayout";
+            this.Text = "TKN Tech N\' PC Parts";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pCPartsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphicsCardsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem motherBoardsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem storageDevicesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem peripheralsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem termsAndConditionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userManualToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.Label footerText;
+    }
+}
+
---
+++ TKNPCPartsStore/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TKN_PC_Layout;
+
+namespace TKNPCPartsStore
+{
+    public partial class Form1 : TKNLayout
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
